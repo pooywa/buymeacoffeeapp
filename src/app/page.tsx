f60048e5c7c6cc91589939aa48dc73bdb4a9c51b
Pooya Shourangiz <44345555+pooywa@users.noreply.github.com>
2024-08-23 01:24:32 +0330

    add connectembed to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { ConnectEmbed } from "@/app/thirdweb";
+import { client } from "./client";
+import { chain } from "./chain";
+
 export default function Home() {
   return (
-    <div>
-      
+    <div className="flex flex-col min-h-screen justify-center items-center">
+      <h1 className="my-4 text-xl">Buy Me A Coffee App</h1>
+      <ConnectEmbed
+        client={client}
+        chain={chain}
+      />
     </div>
   );
 }
