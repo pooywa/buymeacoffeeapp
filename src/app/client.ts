ac0255852ca984acbe29ee8eaf57ae106d465a76
Pooya Shourangiz <44345555+pooywa@users.noreply.github.com>
2024-08-22 23:57:33 +0330

    modify my mistake

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,2 +1,10 @@
-const CLIENT_ID =   process.env.NEXT_PUBLIC_CLIENT_ID;
+import { createThirdwebClient } from 'thirdweb';
 
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+
+
+export const client = createThirdwebClient({
+
+    clientId: CLIENT_ID,
+
+});
