72fb69d4688d4b726c65983fe282bc7aa579e0c3
Pooya Shourangiz <44345555+pooywa@users.noreply.github.com>
2024-08-24 00:09:12 +0330

    complete buymecoffee component

--- components/BuyMeCoffee.tsx
+++ components/BuyMeCoffee.tsx
@@ -3,11 +3,13 @@
 import { chain } from "@/app/chain";
 import { client } from "@/app/client";
 import { useState } from "react";
-import { ConnectButton, useActiveAccount } from "thirdweb/react";
+import { prepareContractCall, toWei } from "thirdweb";
+import { ConnectButton, TransactionButton, useActiveAccount } from "thirdweb/react";
+import { contract } from "../utils/contract";
 
 export const BuyMeCoffee = () => {
     const account = useActiveAccount();
-    
+
     const [tipAmount, setTipAmount] = useState(0);
     const [message, setMessage] = useState("");
 
@@ -18,7 +20,49 @@ export const BuyMeCoffee = () => {
                     client={client}
                     chain={chain}
                 />
+
+                <div>
+                    <label className="block text-sm font-medium">Tip Amount: </label>
+                    <p>*tip must be grater than 0</p>
+                    <input
+                        className="block w-full rounded-md border-1 py-1.5 pl-7 pr-20 text-black"
+                        type="number"
+                        value={tipAmount}
+                        onChange={(e) => setTipAmount(Number(e.target.value))}
+                        step={0.01}
+                    />
+
+                    <label>Message</label>
+                    <input
+                        className="block w-full"
+                        type="text"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        placeholder="write your message"
+                    />
+                </div>
+                {message && tipAmount > 0 && (
+                    <TransactionButton
+                        transaction={() => (
+                            prepareContractCall({
+                                contract: contract,
+                                method: 'buyMeACoffee',
+                                params: [message],
+                                value: BigInt(toWei(tipAmount.toString())),
+                            })
+                        )}
+                        onTransactionConfirmed={() => {
+                            alert("thank you for the coffee");
+                            setTipAmount(0);
+                            setMessage("");
+                        }}
+                    >Buy Me A Coffee</TransactionButton>
+                )}
+
+
             </div>
+
+
         )
     }
 };
